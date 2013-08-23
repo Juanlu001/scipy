3984087c88b216fdb39f7fcd02690fdc3095da15
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2013-08-23 08:49:07 +0200

    Modified rtol and atol in test_lsoda2.py Now all three tests produce desired output.

--- scipy/integrate/tests/test_lsoda2.py
+++ scipy/integrate/tests/test_lsoda2.py
@@ -8,7 +8,10 @@ def fex(t, y):
     ydot[1] = -ydot[0] - ydot[2]
     return ydot
 
-r = ode(fex).set_integrator('lsoda')
+rtol = 1.0e-4
+atol = np.array([1.0e-6, 1.0e-10, 1.0e-6])
+
+r = ode(fex).set_integrator('lsoda', rtol=rtol, atol=atol)
 t0 = 0.0
 y0 = [1.0, 0.0, 0.0]
 r.set_initial_value(y0, t0)
@@ -19,4 +22,5 @@ while r.successful() and r.t < 4e10:
     print("At t = {:.4e}   y = {}".format(r.t, r.y))
     t2 = t2 * 10
 
-print(r._integrator.iwork[10], r._integrator.iwork[11], r._integrator.iwork[12], r._integrator.iwork[18], r._integrator.rwork[14])
+print("No. steps = {}, no. f-s = {}, no. j-s = {}".format(r._integrator.iwork[10], r._integrator.iwork[11], r._integrator.iwork[12]))
+print("method last used = {}, last switch at t = {:.4e}".format(r._integrator.iwork[18], r._integrator.rwork[14]))
